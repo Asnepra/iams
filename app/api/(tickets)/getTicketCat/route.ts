715d5ef4f6c614aeba27ff20067388fbc176b5b5
Ankit Pratap <ankitpratap1995@gmail.com>
2024-10-18 18:02:42 +0530

    made get ticket api

--- app/api/(tickets)/getTicketCat/route.ts
+++ app/api/(tickets)/getTicketCat/route.ts
@@ -4,6 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 const sql = require('mssql');
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import mssqlconnect from "@/lib/mssqlconnect"; // Import your MSSQL connection function
+import { ticketStatus } from "@/schemas/meta-data";
 
 // Define the POST request handler
 export const POST = async (req: NextRequest) => {
@@ -35,27 +36,44 @@ export const POST = async (req: NextRequest) => {
 
         // Fetch ticket categories and subcategories
         const result = await sql.query`
-            SELECT 
-                TICKET_CAT_ID,
-                MAIN_CAT_ID,
-                MAIN_CAT_NAME,
-                SUB_CAT_ID,
-                SUB_CAT_NAME
-            FROM 
-                [IAMS].[dbo].[IAMS_P_TICKET_CAT]
+              SELECT 
+    TICKET.TICKET_ID,
+    TICKET.TICKET_DESC,
+    TICKET.TICKET_STATUS_ID,
+    TICKET.TICKET_RAISED_ON,
+	TICKET.TICKET_CAT_ID,
+	TICKET_ASSIGNED_TO,
+	TICKET_ASSIGNED_ON,
+    EMP.EmployeeNumber,
+    EMP.EmployeeName,
+    EMP.EmpDepartment,
+    EMP.EmpMail,
+    EMP.DESIGNATION,
+    ASSET.ASSET_MAKE,
+    ASSET.ASSET_MODEL,
+	S.TICKET_STATUS_DESCRIPTION,
+	C.MAIN_CAT_NAME,
+	C.SUB_CAT_NAME
+
+FROM 
+    [IAMS].[dbo].[IAMS_X_TICKET] AS TICKET
+JOIN 
+    [IAMS].[dbo].[UserMaster] AS EMP ON TICKET.TICKET_RAISED_BY = EMP.EmployeeNumber
+JOIN 
+    [IAMS].[dbo].[IAMS_M_ASSET] AS ASSET ON TICKET.ASSET_ID = ASSET.ASSET_BATCH_ID
+JOIN [IAMS].[dbo].[IAMS_P_TICKET_STATUS] AS S ON TICKET.TICKET_STATUS_ID=S.TICKET_STATUS_ID
+JOIN [IAMS].[dbo].[IAMS_P_TICKET_CAT] AS C ON TICKET.TICKET_CAT_ID=C.TICKET_CAT_ID
         `;
 
         // Map the SQL result to JSON format
-        const categories = result.recordset.map((record: any) => ({
-            ticketCatId: record.TICKET_CAT_ID,
-            mainCatId: record.MAIN_CAT_ID,
-            mainCatName: record.MAIN_CAT_NAME,
-            subCatId: record.SUB_CAT_ID,
-            subCatName: record.SUB_CAT_NAME,
+        const ticket = result.recordset.map((record: any) => ({
+            ticketId:record.TICKET_ID,
+            ticketStatus:record.TICKET_STATUS_DESCRIPTION,
+            
         }));
 
         // Send JSON response with categories
-        return new NextResponse(JSON.stringify(categories), { status: 200 });
+        return new NextResponse(JSON.stringify(ticket), { status: 200 });
     } catch (error) {
         console.error("Error fetching categories:", error);
         return new NextResponse(
